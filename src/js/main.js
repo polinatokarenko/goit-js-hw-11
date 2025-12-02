4065182ccfe9b3378baff6c8a9e628db8ee917cc
Yurin <yuriinnx@gmail.com>
2025-12-02 17:50:59 +0200

    do the work http request

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,28 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-import getImagesByQuery from "./pixabay-api.js";
+import { getImagesByQuery } from "./pixabay-api.js";
 
+const form = document.querySelector('.form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const query = input.value.trim();
+
+    if (query === "") {
+        return;
+    }
+
+    getImagesByQuery(query)
+        .then(data => {
+            if (!data.hits || data.hits.length === 0) {
+                iziToast.show({
+                    message: 'Sorry, there are no images matching your search query. Please try again!'
+                });
+            } else {
+                return data.hits;
+            }
+        });
+});
